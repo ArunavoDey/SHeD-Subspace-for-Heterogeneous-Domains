f483e24d5e1cd5b30045ad1aed9e27ecc11896ae
ArunavoDey <arunavo071@gmail.com>
2024-09-11 16:34:24 -0500

    fixed source repo name

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = "SHed"
+project_name = "SHED"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
